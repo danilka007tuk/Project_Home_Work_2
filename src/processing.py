a5025140469387a89f8fbbd0b55ec20977cc2d6a
Danil Sorokin <danimaru@yandex.ru>
2024-12-22 14:06:02 +0500

    fixes add dev

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,9 @@
-from typing import Dict, List, Any
+from typing import Dict, List
 
 
 def filter_by_state(last_dict: List[Dict], value_key: str = "EXECUTED") -> List[Dict]:
-    """Ф"""
+    """Принимает список словарей и ключ: state (по умолчанию 'EXECUTED').
+    Возвращает новый список словарей, содержащий словари соответствующих ключ"""
     new_list_dict = []
     for every_dict in last_dict:
         if every_dict["state"] == value_key:
@@ -11,5 +12,7 @@ def filter_by_state(last_dict: List[Dict], value_key: str = "EXECUTED") -> List[
 
 
 def sort_by_date(list_dict: List[Dict], arg_for_sort: bool = True) -> List[Dict]:
+    """Принимает список словарей и параметр сортировки(по умолчанию "True" — 'CANCELED').
+    Функция возвращает новый список, отсортированный по дате(date)"""
     sort_list = sorted(list_dict, key=lambda every_dict: every_dict["date"], reverse=arg_for_sort)
     return sort_list
